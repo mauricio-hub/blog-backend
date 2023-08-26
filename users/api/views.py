06cfe920aeae4bd9f411615b9b56dab2bac6b8e9
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2023-08-26 16:57:22 -0500

    agregando enpoint de recuperacion de datos del usuario

--- users/api/views.py
+++ users/api/views.py
@@ -1,11 +1,40 @@
 from rest_framework import status
-from rest_framework.views import APIView 
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from users.models import User
+from rest_framework.permissions import IsAuthenticated
+from users.api.serializers import UserRegisterSerilizer, UserSerializer
 
 
+#esta vista crea un usuario
 class RegisterView(APIView):
     def post(self, request):
-        print('register user........')
-        return Response(status=status.HTTP_200_OK , data={'message':'register user'})
-    
+        serializer = UserRegisterSerilizer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(data=serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+#esta vista retorna todos los datos del usuario
+# class UserView(APIView):
+#     permission_classes = [ IsAuthenticated ]
+
+#     def get(self,request):
+#         # en request.user tenemos el usuario autenticado
+#         serializer=UserSerializer(request.user)
+#         serializer.data.pop('password')
+#         return Response(serializer.data)
+#esta vista retorna todos los datos del usuario
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        data = serializer.data
+        data.pop('password')  # Elimina el campo de contraseña de los datos
+        return Response(data)   
+
+
+
